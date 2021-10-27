9c52c50c325b19d55d0f5711d8b87c810bd41001
hyenzzang <2017100898@khu.ac.kr>
2021-10-28 02:51:24 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# flutter_everytime_clone
+# Flutter Everytime Clone App (version 01)
+* I only did the front work and I need DB connection and code refactoring.
+* I'm working on it and it will be updated continuously.
+* It's a work for learning *Flutter*.
+
+## Login
+<img width="300" src="https://user-images.githubusercontent.com/64299475/139117825-a7d00b34-1aa3-473a-b493-568f9b4bd1eb.png"> <img width="300" src="https://user-images.githubusercontent.com/64299475/139118118-73283762-571b-4e06-95ee-2ae39b344d01.png">
+
+## Main Page
+<img width="300" src="https://user-images.githubusercontent.com/64299475/139117816-a539a659-e5e2-4b99-b7df-3244af085e48.png"> <img width="300" src="https://user-images.githubusercontent.com/64299475/139117769-c9ab885b-4156-46c6-896c-aa7f7c4128b8.png">
+
+## Scheduling Page
+<img width="300" src="https://user-images.githubusercontent.com/64299475/139117807-8db1af4f-3d15-40ff-8679-0922873e1de6.png">
+
+## My Page
+<img width="300" src="https://user-images.githubusercontent.com/64299475/139117813-27902d10-e647-42e0-8eab-937e1259fd49.png">
+
+## Search
+<img width="300" src="https://user-images.githubusercontent.com/64299475/139117819-3dc43618-544f-4d53-b6b9-e915ff754d20.png">
+
+
+## SNS Page
+<img width="300" src="https://user-images.githubusercontent.com/64299475/139117799-53786899-a659-4f01-8042-95601d1707ef.png">
+
+
+
---
+++ lib/login.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/widgets.dart';
+import 'home_page.dart';
+import 'home_cubit.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'MainPage.dart';
+
+class Login extends StatefulWidget {
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  TextEditingController controller = TextEditingController();
+  TextEditingController controller2 = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      child: Scaffold(
+          backgroundColor: Colors.white,
+          body: Center(child: Builder(builder: (context) {
+            return GestureDetector(
+              onTap: () {
+                FocusScope.of(context).unfocus();
+              },
+              child: SingleChildScrollView(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.alarm, color: Colors.red.shade400, size: 60),
+                      SizedBox(height: 10),
+                      Text('대학생활을 더 편하고 즐겁게',
+                          style: TextStyle(
+                              color: Colors.grey.shade500, fontSize: 17)),
+                      Text('에브리타임',
+                          style: TextStyle(
+                              color: Colors.red.shade300,
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold)),
+                      Form(
+                          child: Theme(
+                              data: ThemeData(
+                                  primaryColor: Colors.redAccent,
+                                  inputDecorationTheme: InputDecorationTheme(
+                                      labelStyle: TextStyle(
+                                          color: Colors.grey, fontSize: 13))),
+                              child: Container(
+                                  padding: EdgeInsets.all(40.0),
+                                  child: Column(children: [
+                                    Container(
+                                      width: 380,
+                                      height: 42,
+                                      decoration: BoxDecoration(
+                                          color: Colors.grey[200],
+                                          borderRadius:
+                                              BorderRadius.circular(50.0)),
+                                      child: Padding(
+                                        padding:
+                                            const EdgeInsets.only(left: 20.0),
+                                        child: TextField(
+                                          controller: controller,
+                                          decoration: InputDecoration(
+                                            border: InputBorder.none,
+                                            hintText: '아이디',
+                                            hintStyle: const TextStyle(
+                                                color: Color(0xb3252532),
+                                                fontWeight: FontWeight.w400,
+                                                fontFamily: "NotoSans",
+                                                fontStyle: FontStyle.normal,
+                                                fontSize: 16.0),
+                                          ),
+                                          keyboardType:
+                                              TextInputType.emailAddress,
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(height: 4.0),
+                                    Container(
+                                      width: 380,
+                                      height: 42,
+                                      decoration: BoxDecoration(
+                                          color: Colors.grey[200],
+                                          borderRadius:
+                                              BorderRadius.circular(50.0)),
+                                      child: Padding(
+                                        padding:
+                                            const EdgeInsets.only(left: 20.0),
+                                        child: TextField(
+                                          controller: controller2,
+                                          decoration: InputDecoration(
+                                            border: InputBorder.none,
+                                            hintText: '비밀번호',
+                                            hintStyle: const TextStyle(
+                                                color: Color(0xb3252532),
+                                                fontWeight: FontWeight.w400,
+                                                fontFamily: "NotoSans",
+                                                fontStyle: FontStyle.normal,
+                                                fontSize: 16.0),
+                                          ),
+                                          keyboardType:
+                                              TextInputType.emailAddress,
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    ButtonTheme(
+                                        child: ElevatedButton(
+                                            onPressed: () {
+                                              if (controller.text == "vaily" &&
+                                                  controller2.text == "1234") {
+                                                Navigator.pushNamed(
+                                                    context, '/start');
+                                              } else {
+                                                showSnackBar(context);
+                                              }
+                                            },
+                                            child: Text('에브리타임 로그인',
+                                                style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontSize: 18)),
+                                            style: ElevatedButton.styleFrom(
+                                                primary:
+                                                    Colors.redAccent.shade200,
+                                                shape: RoundedRectangleBorder(
+                                                    borderRadius:
+                                                        BorderRadius.circular(
+                                                            50)),
+                                                elevation: 0.0,
+                                                padding: EdgeInsets.fromLTRB(
+                                                    110, 10, 100, 10))))
+                                  ])))),
+                      Text('아이디/비밀번호 찾기',
+                          style: TextStyle(
+                              color: Colors.grey.shade500, fontSize: 15)),
+                      SizedBox(height: 10),
+                      Text('회원가입',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold)),
+                    ]),
+              ),
+            );
+          }))),
+      onWillPop: () => Future.value(false),
+    );
+  }
+}
+
+void showSnackBar(BuildContext context) {
+  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+    content: Text(
+      '올바른 정보를 입력해주세요.',
+      textAlign: TextAlign.center,
+    ),
+    duration: Duration(seconds: 2),
+  ));
+}
